c3ba5428107f6cedc096c7abf0fe641dcca09797
William <wzhouwzhou@gmail.com>
2017-08-09 00:49:39 -0400

    Timings

--- src/deps/math_deps.cxx
+++ src/deps/math_deps.cxx
@@ -1,5 +1,28 @@
 #include <node.h>
 #include <iostream>
+#include <chrono>
+struct MeasureTime {
+  using precision = std::chrono::nanoseconds;
+  std::vector<std::chrono::steady_clock::time_point> times;
+  std::chrono::steady_clock::time_point oneLast;
+  void p() {
+    std::cout << "Execution speed (nanoseconds): "
+              << std::chrono::duration_cast<precision>(times.back() - oneLast).count()
+              << "\n";
+  }
+  void m() {
+    oneLast = times.back();
+    times.push_back(std::chrono::steady_clock::now());
+  }
+  void t() {
+    m();
+    p();
+    m();
+  }
+  MeasureTime() {
+    times.push_back(std::chrono::steady_clock::now());
+  }
+};
 
 using v8::Exception;
 using v8::FunctionCallbackInfo;
@@ -42,10 +65,12 @@ double addAll (const FunctionCallbackInfo<Value>& args){
 }
 
 Local<Number> add (const FunctionCallbackInfo<Value>& args){
+  MeasureTime m;
   Isolate* isolate = args.GetIsolate();
   if(checkArgs(isolate, args)){
     double result = addAll(args);
     std::cout<<"Debug: "<<"Math: Add: "<<result<<"\n";
+    m.t();
     return Number::New(isolate, result);
   }
 
@@ -64,10 +89,12 @@ double subtractAll (const FunctionCallbackInfo<Value>& args){
 }
 
 Local<Number> subtract (const FunctionCallbackInfo<Value>& args){
+  MeasureTime m;
   Isolate* isolate = args.GetIsolate();
   if(checkArgs(isolate, args)){
     double result = subtractAll(args);
     std::cout<<"Debug: "<<"Math: Subtract: "<<result<<"\n";
+    m.t();
     return Number::New(isolate, result);
   }
 
@@ -83,8 +110,11 @@ long long roundInternal (long double x){
 }
 
 Local<Number> round (const FunctionCallbackInfo<Value>& args){
+  MeasureTime m;
   Isolate* isolate = args.GetIsolate();
-  if(checkArgsL(1, isolate, args))
+  if(checkArgsL(1, isolate, args)){
+    m.t();
     return Number::New(isolate, roundInternal(args[0]->NumberValue()));
+  }
   return Number::New(isolate, -1);
 }
